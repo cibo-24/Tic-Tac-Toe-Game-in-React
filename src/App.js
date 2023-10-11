f3edad3e595ec7a59f07fdc56795aade1832a8c7
cibo-24 <cibo.kenan@gmail.com>
2023-10-11 17:27:43 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
 import Board from '../src/components/board';
+import { useState } from 'react';
 
 function App() {
+  // X ve O oluşturmak için tahtanın bu bilgiyi tutması için oluşturacağımız yapı.
+  const [bords, setBoards] =  useState(Array(9).fill(null)); // Array(9).fill(null) dokuz elemanlı dizi oluşturur ve her birini null olarak ayarlar.
   return (
     <>
     <div className='board-row'>
-    <Board />
-    <Board  />
-    <Board  />
+    <Board value={bords[0]} />
+    <Board value={bords[1]} />
+    <Board value={bords[2]} />
 
     </div>
     <div className='board-row'>
-    <Board   />
-    <Board   />
-    <Board   />
+    <Board  value={bords[3]} />
+    <Board  value={bords[4]} />
+    <Board  value={bords[5]} />
     </div>
     <div className='board-row'>
-    <Board  />
-    <Board  />
-    <Board  />
+    <Board value={bords[6]} />
+    <Board value={bords[7]} />
+    <Board value={bords[8]} />
     </div>
 
     
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import '../App.css';
-export default function Board () {
+export default function Board ({value}) {
     const [value, setValue] = useState(null);
     function handleClick () {
         setValue('X');
